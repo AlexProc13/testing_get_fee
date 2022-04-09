ceb6ca7ffce3a3724c029144f469bf17d41ee90a
alexproc <alexproc1313@gmail.com>
2022-04-09 12:46:17 +0300

    fee first test

---
+++ tests/Unit/Commission/BaseCommissionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Commission;
+
+use Faker\Factory;
+use App\Services\Client\Client;
+use PHPUnit\Framework\TestCase;
+use App\Services\Commission\BaseCommission;
+
+class BaseCommissionTest extends TestCase
+{
+    public function testGetCommissionCheckRound()
+    {
+        $faker = Factory::create();
+
+        $service = new BaseCommission();
+        $amount = $faker->randomFloat(4, 1, 1000);
+        $countryCode = $faker->countryCode;
+        $currency = $faker->currencyCode;
+        $rate = $faker->randomFloat(4, 1, 3);
+        $givenFee = $service->get($amount, $currency, $countryCode, $rate);
+        $expectedNumberDecimalDigits = 2;
+        $givenNumberDecimalDigits = strlen(substr(strrchr($givenFee, "."), 1));//todo
+        $this->assertEquals($givenNumberDecimalDigits, $expectedNumberDecimalDigits);
+    }
+}
